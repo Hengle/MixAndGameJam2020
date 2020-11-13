27852c05b7300892dfe53826043e326da4f0ba78
andrea teruzzi <andre.teru93@gmail.com>
2020-11-14 00:50:18 +0100

    no message

---
+++ Assets/_Game/Scripts/Data/SC_IngredientType.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+
+[CreateAssetMenu(fileName = "IngredientType 0", menuName = "Game/Ingredient Type", order = 1)]
+public class SC_IngredientType : SerializedScriptableObject
+{
+
+	public static SC_IngredientType[] GetAllIngredientTypes()
+	{
+		return Resources.LoadAll<SC_IngredientType>("");
+	}
+
+}
---
+++ Assets/_Game/Scripts/Data/TypeOfCookingEnum.cs
@@ -0,0 +1,7 @@
+﻿public enum TypeOfCookingEnum
+{
+    row = 0,
+    underdone = 1,
+    welldone = 2,
+    burnt = 3
+}
