847f8320ec3abd76d7ce1859159d1ca22fb3ff7d
andrea teruzzi <andre.teru93@gmail.com>
2020-11-14 15:43:53 +0100

    aggiunta una funzioen per ottenere tutti i need type in ordine

--- Assets/_Game/Scripts/Data/SC_NeedType.cs
+++ Assets/_Game/Scripts/Data/SC_NeedType.cs
@@ -1,8 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [CreateAssetMenu(fileName = "Need 0", menuName = "Game/Need", order = 1)]
 public class SC_NeedType : ScriptableObject
 {
+	public int requiredWeight = 0;
+	public List<SC_Card> unlockedCard = new List<SC_Card>();
+
+
+	public static List<SC_NeedType> GetAllNeed()
+	{
+		return Resources.LoadAll<SC_NeedType>("").OrderBy(x => x.requiredWeight) as List<SC_NeedType>;
+	}
+
 }
