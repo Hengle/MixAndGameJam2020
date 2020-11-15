7f6e8d94cd0466f01fffe78aed088a838c0d3026
MarcoMarchitelli <igvvcdhk@gmail.com>
2020-11-15 16:26:00 +0100

    Sound controls.

---
+++ Assets/_Game/Scripts/SoundController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundController : MonoBehaviour {
+    public AudioMixer mixer;
+
+    private bool music, fx;
+
+    public void ToggleMusic () {
+        music = !music;
+        mixer.SetFloat( "Music", music ? -80f : 0f );
+    }
+
+    public void ToggleFX () {
+        fx = !fx;
+        mixer.SetFloat( "FX", fx ? -80f : 0f );
+    }
+}
